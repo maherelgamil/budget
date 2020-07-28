c1f5b8f6f8429d8d06c52204dca87774ccde88a0
Daniël van Drunen <daniel@pixely.me>
2020-07-28 21:46:08 +0200

    Get rid of UserRepository in favor of more specific actions

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,33 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\CreateUserAction;
 use App\Actions\StoreSpaceInSessionAction;
 use App\Actions\SendVerificationMailAction;
 use App\Models\LoginAttempt;
+use App\Models\User;
 use Illuminate\Http\Request;
 // use App\Http\Controllers\Controller;
 
 use App\Repositories\CurrencyRepository;
 use App\Repositories\LoginAttemptRepository;
 use App\Repositories\SpaceRepository;
-use App\Repositories\UserRepository;
 use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
     private $currencyRepository;
-    private $userRepository;
     private $spaceRepository;
     private $loginAttemptRepository;
 
     public function __construct(
         CurrencyRepository $currencyRepository,
-        UserRepository $userRepository,
         SpaceRepository $spaceRepository,
         LoginAttemptRepository $loginAttemptRepository
     ) {
         $this->currencyRepository = $currencyRepository;
-        $this->userRepository = $userRepository;
         $this->spaceRepository = $spaceRepository;
         $this->loginAttemptRepository = $loginAttemptRepository;
     }
@@ -42,9 +40,9 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate($this->userRepository->getValidationRulesForRegistration());
+        $request->validate(User::getValidationRulesForRegistration());
 
-        $user = $this->userRepository->create($request->name, $request->email, $request->password);
+        $user = (new CreateUserAction())->execute($request->name, $request->email, $request->password);
         $space = $this->spaceRepository->create($request->currency, $user->name . '\'s Space');
         $user->spaces()->attach($space->id, ['role' => 'admin']);
 
---
+++ config/dashboard.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'cards' => [
+        'balance',
+        'spent_daily',
+        'spent_weekly',
+        'spent_weekly'
+    ]
+];
